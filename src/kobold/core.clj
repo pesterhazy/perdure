9be30918518b5b768431989222a2034e98b6b48c
Paulus Esterhazy <pesterhazy@gmail.com>
2015-04-04 15:41:44 +0200

    Hashing objects

--- src/kobold/core.clj
+++ src/kobold/core.clj
@@ -1,6 +1,20 @@
-(ns kobold.core)
+(ns kobold.core
+  (:require [me.raynes.conch :refer [with-programs]]))
 
-(defn foo
-  "I don't do a whole lot."
+(defn to-obj
+  "Takes a string, writes it to the object store. Returns hash"
+  [st]
+  (with-programs [git]
+    (-> (git "hash-object" "--stdin"
+             {:in st})
+        (clojure.string/trim-newline))))
+
+(defn ser
+  "Serializes the argument"
   [x]
-  (println x "Hello, World!"))
+  (if (vector? x)
+    (let [st (->> x
+                  (map ser)
+                  (clojure.string/join))]
+      (print st))
+    (prn-str x)))
