050e0422a72bcab81d1741cf280ca59af0200e8a
Paulus Esterhazy <pesterhazy@gmail.com>
2015-04-05 18:06:53 +0200

    update

--- src/kobold/core.clj
+++ src/kobold/core.clj
@@ -14,25 +14,45 @@
 (defn hash-object
   "Takes a string, writes it to the object store. Returns hash"
   [st]
-  (git [:hash-object "--stdin"] st))
+  (git [:hash-object "-w" "--stdin"] st))
+
+(defn mktree [tree]
+  (git :mktree tree))
 
 (defn cat-file
   [rev]
   (println (git [:cat-file "-p" rev])))
 
-(declare wrt)
+;; (declare wrt)
 
-(defn ser
-  "Serializes the argument"
-  [x]
-  (if (vector? x)
-    (let [result (with-out-str (doseq [e (map wrt x)]
-                                 (println e)))]
-      result)
-    (pr-str x)))
+;; (defn ser
+  ;;   "Serializes the argument"
+;;   [x]
+;;   (if (vector? x)
+;;     (reduce (fn [[outstr hashes] el] (let [[st hsh]])[(str outstr st "\n") (conj hashes hsh)]) ["" #{}] x)
+;;     [(pr-str x) nil]))
 
-(defn wrt
-  [x]
-  (if (vector? x)
-    (str "#rf \"" (hash-object (ser x)) "\"")
-    (ser x)))
+;; (defn wrt
+;;   [x]
+;;   (if (vector? x)
+;;     (let [hsh (hash-object (ser x))]
+;;       [(str "#rf \"" hsh  "\"") hsh])
+;;     [(ser x) nil]))
+
+
+(defn ser-vec [v]
+  (-> (map prn-str v)
+      clojure.string/join))
+
+(defn vec->tree
+  [v]
+  "Converts a vector into a string representation of a git tree object"
+  (let [serialized (ser-vec v)
+        hsh (hash-object serialized)]
+    (str "100644 blob " hsh "\troot")))
+
+(defn write-vec [v]
+  (-> v vec->tree mktree))
+
+(defn read-vec [hsh]
+  (git [:show (str hsh ":root")]))
