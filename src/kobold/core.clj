d6353cb7f9300aa28c7e4407c3cfe16b76b0b49b
Paulus Esterhazy <pesterhazy@gmail.com>
2015-04-05 18:27:10 +0200

    update

--- src/kobold/core.clj
+++ src/kobold/core.clj
@@ -39,17 +39,31 @@
 ;;       [(str "#rf \"" hsh  "\"") hsh])
 ;;     [(ser x) nil]))
 
+(defn ser-el [e]
+  (if (vector? e)
+    (str "#rf \"" (write-vec e) "\"\n")
+    (prn-str e)))
 
-(defn ser-vec [v]
-  (-> (map prn-str v)
-      clojure.string/join))
+(defn ser-vec
+  "Returns a pair [blob tree-str]"
+  [v]
+  (reduce
+   (fn [[out-str out-tree tree-count] e]
+     (if (vector? e)
+       (let [hsh (write-vec e)]
+         [(str out-str (str "#rf \"" hsh "\"\n"))
+          (str out-tree "040000 tree " hsh "\t" tree-count "\n")
+          (inc tree-count)])
+       [(str out-str (prn-str e)) out-tree tree-count]))
+   ["" "" 0]
+   v))
 
 (defn vec->tree
   [v]
   "Converts a vector into a string representation of a git tree object"
-  (let [serialized (ser-vec v)
-        hsh (hash-object serialized)]
-    (str "100644 blob " hsh "\troot")))
+  (let [[blob tree-str] (ser-vec v)
+        hsh (hash-object blob)]
+    (str "100644 blob " hsh "\troot\n" tree-str)))
 
 (defn write-vec [v]
   (-> v vec->tree mktree))
