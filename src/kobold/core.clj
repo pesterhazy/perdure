c8bb3e8738665e40f06bbedc76c6e58669144f75
Paulus Esterhazy <pesterhazy@gmail.com>
2015-04-08 16:24:53 +0200

    More fixing

--- src/kobold/core.clj
+++ src/kobold/core.clj
@@ -9,11 +9,16 @@
    (if (sequential? args)
      (with-programs [git]
        (let [repo "/home/paulus/prg/kobold/rep"
+             ;;_ (println "git" (clojure.string/join " " (map pr-str args)))
              params (concat (map name args) [{:in in, :dir repo}])]
-         ;; (println "git" (clojure.string/join " " params))
          (-> (apply git params)
              (clojure.string/trim-newline))))
      (git [args] in))))
+
+(defn obj-type
+  [hsh]
+  (-> (git [:cat-file "-t" hsh])
+      keyword))
 
 (defn hash-object
   "Takes a string, writes it to the object store. Returns hash"
@@ -26,6 +31,13 @@
 (defn cat-file
   [rev]
   (println (git [:cat-file "-p" rev])))
+
+(defn commit-hash->tree-hash
+  [commit-hash]
+  (assert (= :commit (obj-type commit-hash)))
+  (some->> (git [:cat-file "-p" commit-hash])
+           (re-find #"(?m)^tree ([0-9a-f]*)$")
+           second))
 
 (declare ser-vec)
 (declare ser-map)
@@ -87,21 +99,20 @@
 (defn write-coll
   "Writes a collection to the db. Returns hash of the tree object"
   [v]
-  (-> v coll->tree mktree))
+  (if-let [sha (-> v meta :sha)]
+    (do
+      (println "nothing to do, sha is already known: " sha)
+      sha)
+    (let [sha (-> v coll->tree mktree)]
+      (println "writing collection to disk, sha: " sha)
+      sha)))
 
 ;; ----
 
 (declare read-coll*)
 
-(defn read-blob [dict hsh]
-  (if (@dict hsh)
-    (do
-      (println "already in dict: " hsh)
-      (@dict hsh))
-    (let [blob (git [:show (str hsh ":root")])]
-      (println "reading from storage: " hsh)
-      (swap! dict assoc hsh blob)
-      blob)))
+(defn read-blob [hsh]
+  (git [:show (str hsh ":root")]))
 
 (defn blob->coll
   [dict st]
@@ -110,12 +121,20 @@
 (defn read-coll*
   "Takes the hash of a tree object and reads the collection stored"
   [dict hsh]
-  (->> hsh (read-blob dict) (blob->coll dict)))
+  (with-meta (if (@dict hsh)
+               (do
+                 (println "already in dict: " hsh)
+                 (@dict hsh))
+               (let [v (->> hsh read-blob (blob->coll dict))]
+                 (println "reading from storage: " hsh)
+                 (swap! dict assoc hsh v)
+                 v)) {:sha hsh}))
 
 (defn read-coll-with-dict
   "Returns a pair [updated-hdict coll]"
   [hdict hsh]
-  (let [dict (atom {})]
+  (assert (= :tree (obj-type hsh)))
+  (let [dict (atom hdict)]
     [@dict (read-coll* dict hsh)]))
 
 (defn read-coll
@@ -142,22 +161,19 @@
 
 (defn commit-coll
   [v]
-  (let [hd (read-head)]
-    (-> v write-coll (commit-tree hd) advance-head)))
+  (let [parent (read-head)]
+    (-> v write-coll (commit-tree parent) advance-head)))
 
 (defn show-head
   []
-  (-> (read-head) read-coll second))
+  (-> (read-head) commit-hash->tree-hash read-coll))
 
-(deftype GitBackend [bla]
+(deftype GitBackend [dict-atom]
   IDurableBackend
   (-commit!
     [this new-val]
-    (let [old-val (show-head)]
-      (println "commit!")
-      (println "old: " old-val)
-      (println "new: " new-val)
-      (commit-coll new-val))
+    (println "commiting new val:" new-val)
+    (commit-coll new-val)
     true)
   (-remove!
     [this]
@@ -166,4 +182,8 @@
 
 (defn git-atom
   [init]
-  (atom* (show-head) (GitBackend. nil) {}))
+  (let [[dict val] (->> (read-head)
+                        commit-hash->tree-hash
+                        (read-coll-with-dict {}))
+        dict-atom (atom dict)]
+    (atom* val (GitBackend. dict-atom) {})))
